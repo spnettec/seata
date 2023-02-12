eb8767c63b96cf9877906a07b5bc09981ef9300c
heyoulin <heyoulin@gmail.com>
2023-02-12 18:05:20 +0800

    upgrade

--- common/src/main/java/io/seata/common/loader/EnhancedServiceLoader.java
+++ common/src/main/java/io/seata/common/loader/EnhancedServiceLoader.java
@@ -33,7 +33,7 @@ import io.seata.common.Constants;
 import io.seata.common.executor.Initialize;
 import io.seata.common.util.CollectionUtils;
 import io.seata.common.util.StringUtils;
-import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -401,7 +401,7 @@ public class EnhancedServiceLoader {
             } catch (Throwable e) {
                 throw new EnhancedServiceNotFoundException(
                     "not found service provider for : " + type.getName()
-                        + " caused by " + ExceptionUtils.getFullStackTrace(e));
+                        + " caused by " + ExceptionUtils.getStackTrace(e));
             }
         }
 
@@ -421,7 +421,7 @@ public class EnhancedServiceLoader {
                 } else {
                     throw new EnhancedServiceNotFoundException(
                             "not found service provider for : " + type.getName() + " caused by " + ExceptionUtils
-                                    .getFullStackTrace(e));
+                                    .getStackTrace(e));
                 }
             }
         }
