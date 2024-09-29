6d87e019c10c41eea2e296acb2f4acbcf608dcff
heyoulin <heyoulin@gmail.com>
2024-09-29 12:12:52 +0200

    2.1.2.B

--- config/seata-config-nacos/src/test/java/org/apache/seata/config/nacos/NacosMockTest.java
+++ config/seata-config-nacos/src/test/java/org/apache/seata/config/nacos/NacosMockTest.java
@@ -33,14 +33,11 @@ import org.apache.seata.config.ConfigurationChangeEvent;
 import org.apache.seata.config.ConfigurationChangeListener;
 import org.apache.seata.config.ConfigurationFactory;
 import org.apache.seata.config.Dispose;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.OS;
 
+@Disabled
 public class NacosMockTest {
     private static ConfigService configService;
     private static final String NACOS_ENDPOINT = "127.0.0.1:8848";
